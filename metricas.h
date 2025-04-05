78f4f32feb3651d77cfb25534cb2e5c7a1661cfc
C0nstanzaL <colucero@alumnos.uai.cl>
2025-04-05 00:27:23 -0300

    funcion apo, apd, ims y hp

--- metricas.h
+++ metricas.h
@@ -5,3 +5,15 @@
 
 // Función que lee el archivo CSV y llena el arreglo de ventas
 int leer_csv(const char *filename, Venta ventas[]);
+
+// Función que calcula el promedio de pizzas por orden
+char *apo(int *size, Venta *ventas);
+
+// Función que calcula el promedio de pizzas por día
+char *apd(int *size, Venta *ventas);
+
+// Función que devuelve el ingrediente más vendido
+char *ims(int *size, Venta *ventas);
+
+// Función para Conteo de pizzas por categoría
+char* hp(int *size, Venta *ventas);
