26bcc631d25bda1d4bb96bc1ba3e8203babb4a6f
sofiii-saez <sosaez@alumnos.uai.cl>
2025-04-05 13:23:10 -0300

    struct de la orden

--- structs.h
+++ structs.h
@@ -1,1 +1,21 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct {
+    int pizza_id;
+    int order_id;
+    char pizza_name_id[100];
+    int quantity;
+    char order_date[20];
+    char order_time[20];
+    float unit_price;
+    float total_price;
+    char pizza_size[5];
+    char pizza_category[50];
+    char pizza_ingredients[200];
+    char pizza_name[100];
+} Venta;
+
+
+#endif
 
